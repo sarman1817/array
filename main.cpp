bdc7b7f3e34973fd1d09c9e4458a6d9bb7d6fde0
gyok <gyok@ya.ru>
2019-10-13 17:06:54 +0300

    valera

--- main.cpp
+++ main.cpp
@@ -2,8 +2,34 @@
 
 using namespace std;
 
+// write yourself
+void ShowArray(size_t* i_a, size_t i_size)
+{
+    for (size_t index = 0; index < i_size; index++){
+        cout<< " "<< i_a[index];
+    }
+}
+
+void ZeroArray(size_t* i_a, size_t i_size) {
+    for (size_t index =0; index <i_size;index++){i_a[index]=0;}
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    const size_t size = 18;
+    size_t  a[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        a[i] = i;
+    }
+
+    ShowArray(a, size);
+
+    cout << endl << "==============" << endl;
+    ZeroArray(a, 5);
+
+    ShowArray(a, size);
+    cout << endl;
+
     return 0;
 }
